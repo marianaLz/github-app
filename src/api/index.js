34ccb852bf59fa674b965cda678810e69566538d
marianaLz <marianaglp15@gmail.com>
2024-09-06 02:02:18 -0600

    feat: repositories search

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const fetchUsers = (username) => {
   return axios.get(`https://api.github.com/search/users?q=${username}`);
 };
+
+export const fetchRepositories = (repoName) => {
+  return axios.get(`https://api.github.com/search/repositories?q=${repoName}`);
+};
---
+++ src/pages/RepositorySearch.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { Container, Flex, Heading, SimpleGrid } from '@chakra-ui/react';
+
+import Alert from '../components/common/Alert';
+import Loader from '../components/common/Loader';
+import SearchBar from '../components/common/SearchBar';
+import RepoCard from '../components/cards/RepoCard';
+
+import { fetchRepositories } from '../api';
+
+const RepositorySearch = () => {
+  const [repoName, setRepoName] = useState('');
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['repositories', repoName],
+    queryFn: () => fetchRepositories(repoName),
+    enabled: !!repoName,
+  });
+
+  return (
+    <Container
+      align='center'
+      as={Flex}
+      flexDir='column'
+      gap='8'
+      maxW='container.lg'
+      py={10}
+    >
+      <Heading as='h1'>Search GitHub Repositories</Heading>
+
+      <SearchBar
+        value={repoName}
+        onChange={(e) => setRepoName(e.target.value)}
+      />
+
+      {isLoading && <Loader />}
+
+      {error && <Alert message={`Error: ${error.message}`} status='error' />}
+
+      {data?.data?.items?.length === 0 && repoName ? (
+        <Alert message={`No repositories found for "${repoName}"`} />
+      ) : (
+        <SimpleGrid columns={{ base: 2, md: 3, lg: 4 }} spacing='8'>
+          {data?.data?.items?.map((repo) => (
+            <RepoCard key={repo.id} {...repo} />
+          ))}
+        </SimpleGrid>
+      )}
+    </Container>
+  );
+};
+
+export default RepositorySearch;
