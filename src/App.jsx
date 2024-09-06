0a6bcb0aa26225e879d78f9b230555bcd585e508
marianaLz <marianaglp15@gmail.com>
2024-09-06 02:37:44 -0600

    enhancement: lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+import { Suspense, lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-import Home from './pages/Home';
-import RepositorySearch from './pages/RepositorySearch';
-import UserSearch from './pages/UserSearch';
+import Loader from './components/common/Loader';
+
+const Home = lazy(() => import('./pages/Home'));
+const UserSearch = lazy(() => import('./pages/UserSearch'));
+const RepositorySearch = lazy(() => import('./pages/RepositorySearch'));
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/repositories' element={<RepositorySearch />} />
-      <Route path='/users' element={<UserSearch />} />
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/repositories' element={<RepositorySearch />} />
+        <Route path='/users' element={<UserSearch />} />
+      </Routes>
+    </Suspense>
   );
 }
 
